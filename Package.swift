cb8433496fbff102ae690368a3a8cb3e766161fe
Jan-Erik Rediger <janerik@fnordig.de>
2021-05-18 12:25:07 +0200

    Use temporary URL

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let checksum = "f3115195f9441b26adc1e07cd1c616b79b81627caa066a9bc987484c14727f4c"
 let version = "38.0.0"
-let url = "https://github.com/mozilla/glean-swift/releases/download/\(version)/Glean.xcframework.zip"
+let url = "https://github.com/badboy/glean-swift/releases/download/\(version)/Glean.xcframework.zip"
 
 let package = Package(
     name: "Glean",
@@ -12,6 +12,6 @@ let package = Package(
         .library(name: "Glean", targets: ["Glean"]),
     ],
     targets: [
-        .binaryTarget(name: "Glean", path: "./archives/Glean.xcframework"),
+        .binaryTarget(name: "Glean", url: url, checksum: checksum),
     ]
 )
