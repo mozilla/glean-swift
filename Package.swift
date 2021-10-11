ba1fedc1e8c94d95f2ccbfb2aefd8836b0df3256
Glean automated release <jrediger@mozilla.com>
2021-10-11 09:36:25 +0000

    Bumped version to 42.0.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.4
 import PackageDescription
 
-let checksum = "7cd42c693eb64ac22594e4f4aa81f1f3ce95b3445e8be5d267f5ec8f9e768e74"
-let version = "42.0.0"
+let checksum = "309266e2c54a2c4f8d4e96f2fa37a551f4d8a0f1a30e5f83361ac8389d3566c9"
+let version = "42.0.1"
 let url = "https://github.com/mozilla/glean/releases/download/v\(version)/Glean.xcframework.zip"
 
 let package = Package(
