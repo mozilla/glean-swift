e27fd6a7afad9622f227feb5d04e5192d3ff7ff1
Glean automated release <jrediger@mozilla.com>
2024-05-31 12:58:32 +0000

    Bumped version to 60.0.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version:5.5
 import PackageDescription
 
-let checksum = "a08d5c2d0c2ff6992e22aaf56a2f552ad57e26113c49682c06f59134ea40cbb9"
-let version = "60.3.0"
+let checksum = "1d6acb00115259de0fc9b25fa9229d2391491b849af58db96974565ebe79a997"
+let version = "60.0.1"
 let url = "https://github.com/mozilla/glean/releases/download/v\(version)/Glean.xcframework.zip"
 
 let package = Package(
